688d0d5ad377f0ca437bfb35833bd750aa9fa7cc
Hussein Elghoul <h.elghoul.c@rcu.gov.sa>
2024-11-13 20:54:08 +0200

    changed sql scripts

---
+++ sql/bookbliss.sql
@@ -0,0 +1,134 @@
+USE DATABASE bookbliss
+DROP TABLE IF EXISTS `addresses`;
+CREATE TABLE `addresses` (
+  `address_id` int NOT NULL AUTO_INCREMENT,
+  `city` varchar(50) NOT NULL,
+  `street_name` varchar(50) NOT NULL,
+  `building_name` varchar(50) NOT NULL,
+  `floor_number` int NOT NULL,
+  `zipcode` varchar(20) DEFAULT NULL,
+  `details` text,
+  `customer_id` int NOT NULL,
+  PRIMARY KEY (`address_id`),
+  KEY `customer_id` (`customer_id`),
+  CONSTRAINT `addresses_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`)
+)
+
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE `admin` (
+  `admin_id` int NOT NULL AUTO_INCREMENT,
+  `password_hash` varchar(256) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone_number` varchar(20) DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`admin_id`),
+  UNIQUE KEY `email` (`email`)
+)
+
+DROP TABLE IF EXISTS `books`;
+CREATE TABLE books (
+  `book_id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `author` varchar(100) NOT NULL,
+  `genre` enum('Self help','Psychology','Personal Finance','Romance','Fiction','Horror') NOT NULL,,
+  `isbn` varchar(20) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `stock` int NOT NULL,
+  `published_date` date DEFAULT NULL,
+  `description` text,
+  `cover_image_url` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`book_id`),
+  UNIQUE KEY `isbn` (`isbn`)
+)
+
+DROP TABLE IF EXISTS cart_items;
+CREATE TABLE `cart_items` (
+  `cart_item_id` int NOT NULL AUTO_INCREMENT,
+  `cart_id` int NOT NULL,
+  `book_id` int NOT NULL,
+  `quantity` int NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`cart_item_id`),
+  KEY `cart_id` (`cart_id`),
+  KEY `book_id` (`book_id`),
+  CONSTRAINT `cartitems_ibfk_1` FOREIGN KEY (`cart_id`) REFERENCES `carts` (`cart_id`),
+  CONSTRAINT `cartitems_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`)
+)
+
+DROP TABLE IF EXISTS carts;
+CREATE TABLE `carts` (
+  `cart_id` int NOT NULL AUTO_INCREMENT,
+  `customer_id` int NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`cart_id`),
+  KEY `customer_id` (`customer_id`),
+  CONSTRAINT `carts_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`)
+)
+
+DROP TABLE IF EXISTS `customers`;
+CREATE TABLE `customers` (
+  `customer_id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `password_hash` varchar(256) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone_number` varchar(20) DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`customer_id`),
+  UNIQUE KEY `email` (`email`)
+)
+
+DROP TABLE IF EXISTS `gift_cards`;
+CREATE TABLE `gift_cards` (
+  `gift_card_id` int NOT NULL AUTO_INCREMENT,
+  `code` varchar(50) NOT NULL,
+  `amount` decimal(10,2) NOT NULL,
+  `expiry_date` date NOT NULL,
+  `is_redeemed` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`gift_card_id`),
+  UNIQUE KEY `code` (`code`)
+)
+
+DROP TABLE IF EXISTS `order_items`;
+CREATE TABLE order_items (
+  `order_item_id` int NOT NULL AUTO_INCREMENT,
+  `order_id` int NOT NULL,
+  `book_id` int NOT NULL,
+  `quantity` int NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`order_item_id`),
+  KEY `order_id` (`order_id`),
+  KEY `book_id` (`book_id`),
+  CONSTRAINT `orderitems_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`),
+  CONSTRAINT `orderitems_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`)
+)
+
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE orders (
+  `order_id` int NOT NULL AUTO_INCREMENT,
+  `customer_id` int NOT NULL,
+  `order_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `total_amount` decimal(10,2) NOT NULL,
+  `payment_method` enum('Card','Cash on Delivery','Gift Card') NOT NULL,
+  `gift_card_id` int DEFAULT NULL,
+  PRIMARY KEY (`order_id`),
+  KEY `customer_id` (`customer_id`),
+  KEY `gift_card_id` (`gift_card_id`),
+  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`),
+  CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`gift_card_id`) REFERENCES `gift_cards` (`gift_card_id`)
+)
+
+DROP TABLE IF EXISTS reviews;
+CREATE TABLE reviews (
+  `review_id` int NOT NULL AUTO_INCREMENT,
+  `customer_id` int NOT NULL,
+  `book_id` int NOT NULL,
+  `rating` int NOT NULL,
+  `review_text` text,
+  `review_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`review_id`),
+  KEY `customer_id` (`customer_id`),
+  KEY `book_id` (`book_id`),
+  CONSTRAINT `reviews_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`),
+  CONSTRAINT `reviews_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`),
+  CONSTRAINT `reviews_chk_1` CHECK ((`rating` between 1 and 5))
