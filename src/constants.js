3e3be4ce95d940464a7f0cc9120e0966d02719e2
Hawraa Slaimen <hawraaslayman9@gmail.com>
2024-11-21 16:22:41 +0200

    first commit

--- src/constants.js
+++ src/constants.js
@@ -2,31 +2,31 @@ module.exports.categoryList = [
     {
         id: 'personal-finance',
         name: 'Finance',
-        description: 'Take control of your financial future with our personal finance book collection!',
+        description: 'Master your finances with our books, packed with tips and strategies to achieve your goals!',
         image: '/images/Finance.jpg'
     },
     {
         id: 'psychology',
         name: 'Psychology',
-        description: 'Dive into our psychology book collection and unlock the secrets of the mind!',
-        image: '/images/psychology.jpg'
+        description: 'Explore our psychology books for insights into how we think and feel. ',
+        image: '/images/p6jpg.jpg'
     },
     {
         id: 'romance',
         name: 'Romance',
-        description: 'Fall in love with reading through our enchanting romance book collection!',
+        description: 'Fall in love with our romance books, featuring heartwarming and passionate stories.',
         image: '/images/love1jpg.jpg'
     },
     {
         id: 'self-help',
         name: 'SelfHelp',
-        description: 'Transform your life with our self-help collection!',
+        description: 'Transform your life with our self-help books, your guide to growth and living your best life!',
         image: '/images/selfHelp.jpg'
     },
     {
         id: 'Fiction',
         name: 'Fiction',
-        description: 'Explore the darkest corners of imagination with our horror collection!',
+        description: 'Escape into new worlds with our fiction books, offering mysteries, adventures, and heartfelt dramas.',
         image: '/images/Fiction.jpg'
     },
     {
